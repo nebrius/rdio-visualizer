23822ac4ad1ca921da2a796544d27d6486dea7a0
Bryan Hughes <bryan.hughes@rd.io>
2015-01-23 15:01:08 -0800

    Added CORS support

--- server/index.js
+++ server/index.js
@@ -19,6 +19,12 @@ serialPort.on('open', function (err) {
 
   app.use(bodyParser.urlencoded({extended: false}));
   app.use(bodyParser.json());
+  app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+  });
 
   /*var lightState = 0;
   setInterval(function() {
